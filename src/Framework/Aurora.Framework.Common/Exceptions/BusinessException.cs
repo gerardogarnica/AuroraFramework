60b9f64c5583ee3ce90c203691279c4885da2dcd
gerardogarnica <gerardo.garnica@gmail.com>
2021-11-19 10:47:01 -0500

    Aurora.Framework.Common: remove ErrorCategory property in BusinessException class.

--- src/Framework/Aurora.Framework.Common/Exceptions/BusinessException.cs
+++ src/Framework/Aurora.Framework.Common/Exceptions/BusinessException.cs
@@ -9,20 +9,11 @@ namespace Aurora.Framework.Exceptions
     {
         #region Miembros privados de la clase
 
-        private readonly string mErrorCategory;
         private readonly string mErrorType;
 
         #endregion
 
         #region Propiedades de la clase
-
-        /// <summary>
-        /// Categoría o descripción de la excepción.
-        /// </summary>
-        public string ErrorCategory
-        {
-            get { return mErrorCategory; }
-        }
 
         /// <summary>
         /// Tipo de la excepción.
@@ -45,29 +36,25 @@ namespace Aurora.Framework.Exceptions
         /// <summary>
         /// Inicializa una nueva instancia de la clase BusinessException con el mensaje de error especificado.
         /// </summary>
-        /// <param name="category">Categoría o descripción de la excepción.</param>
         /// <param name="errorType">Tipo de la excepción.</param>
         /// <param name="message">Mensaje de error que explica la razón de la excepción.</param>
-        public BusinessException(string category, string errorType, string message)
+        public BusinessException(string errorType, string message)
             : base(message)
         {
             mErrorType = errorType;
-            mErrorCategory = category;
         }
 
         /// <summary>
         /// Inicializa una nueva instancia de la clase BusinessException con el mensaje de error especificado
         /// y una referencia a la excepción interna que representa la causa de la excepción.
         /// </summary>
-        /// <param name="category">Categoría o tipo de la excepción.</param>
         /// <param name="errorType">Código identificativo de la excepción.</param>
         /// <param name="message">Mensaje de error que explica la razón de la excepción.</param>
         /// <param name="innerException">Excepción que ha provocado que se produzca esta excepción.</param>
-        public BusinessException(string category, string errorType, string message, Exception innerException)
+        public BusinessException(string errorType, string message, Exception innerException)
             : base(message, innerException)
         {
             mErrorType = errorType;
-            mErrorCategory = category;
             Source = innerException.Source;
         }
 
