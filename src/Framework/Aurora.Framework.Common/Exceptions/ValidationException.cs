321c7c3c42f7a5f488bee484a24d660d23dc9e57
gerardogarnica <gerardo.garnica@gmail.com>
2021-11-21 10:12:35 -0500

    Aurora.Framework.Common: add ValidationException class.

---
+++ src/Framework/Aurora.Framework.Common/Exceptions/ValidationException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aurora.Framework.Exceptions
+{
+    /// <summary>
+    /// Representa los errores de validaciones de modelos de entrada.
+    /// </summary>
+    public class ValidationException : Exception
+    {
+        /// <summary>
+        /// Lista de mensajes de errores.
+        /// </summary>
+        public List<string> Errors { get; }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase ValidationException.
+        /// </summary>
+        public ValidationException()
+            : base("One or more validation failures have occurred.")
+        {
+            Errors = new List<string>();
+        }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase ValidationException con una lista de mensajes de errores especificados.
+        /// </summary>
+        /// <param name="errors">Lista de mensajes de errores.</param>
+        public ValidationException(IEnumerable<string> errors)
+            : this()
+        {
+            foreach (var error in errors)
+            {
+                Errors.Add(error);
+            }
+        }
+    }
+}
