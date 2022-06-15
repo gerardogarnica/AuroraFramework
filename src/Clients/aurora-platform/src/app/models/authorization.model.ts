1d35e2b8492afc308eff3218df7b629b12f23b9b
gerardo@aktiun.com <gerardo@aktiun.com>
2022-06-15 17:12:50 -0500

    Revert "Aurora Platform Web Admin: adding new project."
    This reverts commit 6464fbfc1d6034d5ed6312c83b0bbc41459b41a3.

--- src/Clients/aurora-platform/src/app/models/authorization.model.ts
+++
@@ -1,3 +0,0 @@
-export interface Authorization {
-    accessToken: string;
-}
--- src/Clients/aurora-platform/src/app/models/module-card.model.ts
+++
@@ -1,7 +0,0 @@
-export interface ModuleCard {
-    title: string;
-    subtitle: string;
-    avatar: string;
-    content: string;
-    routerLink: string;
-}
